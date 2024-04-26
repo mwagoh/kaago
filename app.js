664eee45384ee67c80e1af942d6cc55feb486438
mwagoh <useremail>
2024-04-26 10:37:56 +0300

    files added

---
+++ app.js
@@ -0,0 +1,23 @@
+let num1= 15
+
+
+let num2= 5
+
+//let num3 = num1 - num2
+
+//console.log(num3);
+
+//let num4 = num1 / num2
+
+//console.log(num4);
+
+//let num5 = num1*num2
+
+//console.log(num5);
+
+//let num6 = num1%num2
+
+//console.log(num6);
+
+
+
